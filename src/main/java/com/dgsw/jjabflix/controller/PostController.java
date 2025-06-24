0a8601bce788610923cdba63002ea2de45757e2a
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-06-24 14:52:25 +0900

    😘

--- src/main/java/com/dgsw/jjabflix/controller/PostController.java
+++ src/main/java/com/dgsw/jjabflix/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.dgsw.jjabflix.controller;
 
+import com.dgsw.jjabflix.entity.PostEntity;
 import lombok.RequiredArgsConstructor;
 import com.dgsw.jjabflix.dto.UploadRequest;
 import com.dgsw.jjabflix.service.PostService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("/posts")
 @RequiredArgsConstructor
@@ -18,5 +21,15 @@ public class PostController {
     @DeleteMapping("/delete")
     public String delete(@RequestParam Long id) {
         return service.deletePost(id);
+    }
+
+    @GetMapping("/item")
+    public List<String> item() {
+        return service.getPost();
+    }
+
+    @GetMapping("/item/{id}")
+    public PostEntity findById(@PathVariable Long id) {
+        return service.getPost(id);
     }
 }
--- src/main/java/com/dgsw/jjabflix/service/PostService.java
+++ src/main/java/com/dgsw/jjabflix/service/PostService.java
@@ -6,7 +6,9 @@ import com.dgsw.jjabflix.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +30,14 @@ public class PostService {
             return "deleted";
         }
         return "fail";
+    }
+
+    public List<String> getPost() {
+       return repository.findAll().stream().map(PostEntity::getTitle).collect(Collectors.toList());
+    }
+
+    public PostEntity getPost(Long id) {
+        Optional<PostEntity> entity = repository.findById(id);
+        return entity.orElse(null);
     }
 }
