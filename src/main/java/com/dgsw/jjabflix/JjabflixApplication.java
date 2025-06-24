7d1b8a1790fade9d4d01dfa4f3c7cd9fcf1a4c6a
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-06-24 13:41:28 +0900

    Initial commit

---
+++ src/main/java/com/dgsw/jjabflix/JjabflixApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.jjabflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjabflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjabflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/jjabflix/JjabflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.jjabflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjabflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
