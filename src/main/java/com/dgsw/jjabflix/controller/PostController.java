65aa7f8dfa09a98d85d82e3f1b67ae1e58c360d8
김민준 <kmjons01@gmail.com>
2025-06-24 13:58:16 +0900

    first commit

---
+++ src/main/java/com/dgsw/jjabflix/controller/PostController.java
@@ -0,0 +1,7 @@
+package com.dgsw.jjabflix.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+}
---
+++ src/main/java/com/dgsw/jjabflix/entity/PostEntity.java
@@ -0,0 +1,19 @@
+package com.dgsw.jjabflix.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="Posts")
+public class PostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String content;
+}
